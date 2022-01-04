94b1eb0d71411eb054fa57515eb85b53ca9176b9
Liam Crilly <liam.crilly@nginx.com>
2022-01-04 12:23:35 +0000

    Better instructions

--- macos-prep.sh
+++ macos-prep.sh
@@ -1,41 +1,36 @@
 #!/usr/bin/env bash
 
-echo -n "*** Checking pre-requisites..."
+printf "${0##*/}: INFO: Checking system pre-requisites\n"
 MISSING=""
-for lang in brew java ruby python3; do
-        hash $lang 2> /dev/null
+for dep in brew java ruby python3; do
+        hash $dep 2> /dev/null
         if [ $? -ne 0 ]; then
-                MISSING="$lang $MISSING"
+                MISSING="$dep $MISSING"
         fi
 done
 if [ "$MISSING" != "" ]; then
-    echo " failed"
-    echo "PLEASE INSTALL $MISSING"
+    printf "${0##*/}: ERROR: Missing system pre-requisites, please install $MISSING\n"
     exit 1
 fi
-echo " ok ***"
 
-echo "*** Installing languages pre-requisites ***"
+printf "${0##*/}: INFO: Installing languages pre-requisites\n"
 brew install node golang
 
-echo "*** Installing NGINX Unit"
+printf "\n${0##*/}: INFO: Installing NGINX Unit\n"
 brew install nginx/unit/unit && brew install unit-java unit-python3 unit-ruby
 
-echo "*** Configuring Node.JS division service ***"
+printf "\n${0##*/}: INFO: Configuring Node.JS division service\n"
 cd divide
-npm install body
-chmod +x divide.js
-npm link unit-http
+npm install body && npm link unit-http
 cd -
 
-echo "*** Configuring Golang square root service ***"
+printf "\n${0##*/}: INFO: Configuring Golang square root service\n"
 cd sqroot
-go mod init unit-calculator/sqroot
-go get unit.nginx.org/go
+go mod init unit-calculator/sqroot && go get unit.nginx.org/go
 go build -o sqroot sqroot.go
 cd -
 
-echo "*** Creating local Unit configuration for $PWD as unitconf_local.json ***"
+printf "\n${0##*/}: INFO: Creating local Unit configuration for $PWD as unitconf_local.json\n"
 sed "s|/var/www/unit-calculator|${PWD}|g" < unitconf.json > unitconf_local.json
 
-echo "*** Ready ***"
+printf "${0##*/}: INFO: Ready to start\n"
